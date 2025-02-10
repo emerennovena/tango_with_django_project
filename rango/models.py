a9bcfc39e61605ebe7f89b0b36dd8209e645bed4
Emerentia Novena <3058284n@student.gla.ac.uk>
2025-02-10 14:29:28 +0000

    updated

--- rango/models.py
+++ rango/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.template.defaultfilters import slugify
-# Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
